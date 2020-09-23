8dd8d788cae873c364b58d3586e572d0ef58fda1
captainspockears <ruthuparna1998@gmail.com>
2020-09-23 12:03:46 +0530

    fixed sql error

--- chatbot.py
+++ chatbot.py
@@ -188,7 +188,7 @@ def report():
 
         cur.execute(appendstring)
         conn.commit()
-        conn.close()  
+        #conn.close()  
 
     with open("currmessage.pickle", "wb") as f:
         pickle.dump(("", ""), f)
